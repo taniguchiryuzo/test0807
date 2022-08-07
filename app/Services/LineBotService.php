8f09baa8cd5c720c518e2b2ecde27af2540f1adc
taniguchiryuzo <kousei0038@gmail.com>
2022-08-07 23:51:35 +0900

    validate追加

--- app/Services/LineBotService.php
+++ app/Services/LineBotService.php
@@ -36,6 +36,33 @@ class LineBotService
      */
     public function eventHandler(Request $request): int
     {
+        $this->validateSignature($request);
+
         return 200;
+    }
+
+    /**
+     * LINEの署名確認
+     *
+     * @param Request
+     * @return void
+     * @throws HttpException
+     */
+    public function validateSignature(Request $request): void
+    {
+        // リクエストヘッダーについてくる実際の署名
+        $signature = $request->header('x-line-signature');
+        if ($signature === null) {
+            abort(400);
+        }
+
+        // LINEチャネルシークレットとリクエストボディを基に署名を生成
+        $hash = hash_hmac('sha256', $request->getContent(), config('app.line_channel_secret'), true);
+        $expect_signature = base64_encode($hash);
+
+        // 実際の署名と生成した署名が同じであれば検証OK
+        if (!hash_equals($expect_signature, $signature)) {
+            abort(400);
+        }
     }
 }
