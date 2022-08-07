8cd82c810e962c17666cfbd6a394a8ac375428f0
taniguchiryuzo <kousei0038@gmail.com>
2022-08-08 00:31:32 +0900

    line変更

--- app/Services/LineBotService.php
+++ app/Services/LineBotService.php
@@ -36,6 +36,57 @@ class LineBotService
      */
     public function eventHandler(Request $request): int
     {
-        return 200;
+        // return 200;
+
+        // 署名を検証しLINE以外からのリクエストを受け付けない。
+        $this->validateSignature($request);
+
+        // リクエストをEventオブジェクトに変換する
+        $events = $this->bot->parseEventRequest($request->getContent(), $request->header('x-line-signature'));
+
+        foreach ($events as $event) {
+            // Reply token無しでは返信できないため定義しておく
+            $reply_token = $event->getReplyToken();
+            // 無効な操作があったときに送るメッセージ
+            $message_builder = new TextMessageBuilder('Invalid operation. 無効な操作です。');
+            // アクションした人のLINEのユーザーID
+            $line_user_id = $event->getUserId();
+
+            switch (true) {
+                    // テキストメッセージを受信した場合
+                case $event instanceof TextMessage:
+                    break;
+                    // 選択肢を選んだ場合
+                case $event instanceof PostbackEvent:
+                    break;
+            }
+        }
+
+        // LINEに返信
+        $response = $this->bot->replyMessage($reply_token, $message_builder);
+
+        // 送信に失敗したらログに吐いておく
+        if (!$response->isSucceeded()) {
+            \Log::error('Failed!' . $response->getHTTPStatus() . ' ' . $response->getRawBody());
+        }
+
+        return $response->getHTTPStatus();
+    }
+    public function validateSignature(Request $request): void
+    {
+        // リクエストヘッダーについてくる実際の署名
+        $signature = $request->header('x-line-signature');
+        if ($signature === null) {
+            abort(400);
+        }
+
+        // LINEチャネルシークレットとリクエストボディを基に署名を生成
+        $hash = hash_hmac('sha256', $request->getContent(), config('app.line_channel_secret'), true);
+        $expect_signature = base64_encode($hash);
+
+        // 実際の署名と生成した署名が同じであれば検証OK
+        if (!hash_equals($expect_signature, $signature)) {
+            abort(400);
+        }
     }
 }
